2d1ba1d28cbf6d1f5163d4535d57ef71526223c0
beer-1 <beer-1@codingcats.xyz>
2024-11-08 20:12:55 +0900

    upgrade handler

--- app/upgrade.go
+++ app/upgrade.go
@@ -5,15 +5,31 @@ import (
 
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+
+	opchildtypes "github.com/initia-labs/OPinit/x/opchild/types"
 )
 
-const upgradeName = "0.6.0"
+const upgradeName = "0.6.2"
 
 // RegisterUpgradeHandlers returns upgrade handlers
 func (app *MinitiaApp) RegisterUpgradeHandlers(cfg module.Configurator) {
-	app.UpgradeKeeper.SetUpgradeHandler(upgradeName,
-		func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-			return fromVM, nil
+	app.UpgradeKeeper.SetUpgradeHandler(
+		upgradeName,
+		func(ctx context.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+			params, err := app.OPChildKeeper.GetParams(ctx)
+			if err != nil {
+				return nil, err
+			}
+
+			// set non-zero default values for new params
+			params.HookMaxGas = opchildtypes.DefaultHookMaxGas
+
+			err = app.OPChildKeeper.SetParams(ctx, params)
+			if err != nil {
+				return nil, err
+			}
+
+			return vm, nil
 		},
 	)
 }
